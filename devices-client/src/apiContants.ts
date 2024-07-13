3676f82fa533bd52b8fe1646973a45db0a5a64cb
guiazca <guiazca@gmail.com>
2024-07-13 10:41:20 +0100

    fixnetwork

--- devices-client/src/apiContants.ts
+++ devices-client/src/apiContants.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = `http:/app-network:5000/api`;
+export const API_URL = `http://app-network:5000/api`;
 
 export const api = axios.create({
     baseURL: API_URL,
